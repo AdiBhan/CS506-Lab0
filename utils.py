5be11b40c94513f173c18a64d44c1d58075e9a08
Adi Bhan <abhan03@bu.edu>
2024-09-13 11:01:58 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -28,7 +28,7 @@ def cosine_similarity(v1, v2):
 
     # Hint: Use `dot_product` and `np.linalg.norm`.
     '''
-    return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v1))
+    return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
 
 
 def nearest_neighbor(target_vector, vectors):
